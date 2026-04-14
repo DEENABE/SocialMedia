360d3c26f8c3b12c64510984662c7ef4507919a4
DEENABE <92077829+DEENABE@users.noreply.github.com>
2026-04-14 13:37:58 +0530

    Set default server port to 8000

--- server/server.js
+++ server/server.js
@@ -25,6 +25,6 @@ app.use('/api/post', postRouter)
 app.use('/api/story', storyRouter)
 app.use('/api/message', messageRouter)
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
